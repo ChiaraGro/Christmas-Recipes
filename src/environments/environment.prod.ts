8aa2dadf273d4c885b4c46d0c0eb6ad6426a5c8d
Chiara Grottini <chiaragrottini@gmail.com>
2022-01-04 22:18:51 +0100

    added api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.spoonacular.com/recipes/',
   apiKey2: '2aead72f173d4f9883c3b426401e038a',
-  apiKey: '92c4ef5eace1493c835fcc6edd07d8da'
+  // apiKey: '92c4ef5eace1493c835fcc6edd07d8da'
+  apiKey:'6ce68a1d192c47afa315c166201bbdbe'
 };
